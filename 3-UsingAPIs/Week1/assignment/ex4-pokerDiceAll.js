0768347ce636f29366c62a4302548bebfb3a9587
Hossein <Hossein.kelisa@gmail.com>
2025-01-29 20:47:20 +0100

    Assignments W1-API

--- 3-UsingAPIs/Week1/assignment/ex4-pokerDiceAll.js
+++ 3-UsingAPIs/Week1/assignment/ex4-pokerDiceAll.js
@@ -27,9 +27,8 @@ exercise file.
 import { rollDie } from '../../helpers/pokerDiceRoller.js';
 
 export function rollDice() {
-  // TODO Refactor this function
   const dice = [1, 2, 3, 4, 5];
-  return rollDie(1);
+  return Promise.all(dice.map((die) => rollDie(die)));
 }
 
 function main() {
@@ -43,4 +42,5 @@ if (process.env.NODE_ENV !== 'test') {
   main();
 }
 
-// TODO Replace this comment by your explanation that was asked for in the assignment description.
+// If one promise is rejected, the other dice keep rolling.
+// Each rollDie function runs separately, so rejecting one doesn’t affect the others.
